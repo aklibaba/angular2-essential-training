83e8c2cd7386017665b8f835b72ab3f434c57871
Aleksander <aleksander@direktpoint.pl>
2016-12-21 16:48:56 +0100

    initial set up for NgModule

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import { BrowserModule} from "@angular/platform-browser";
+
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [],
+    bootstrap: []
+})
+export class root {
+
+}
